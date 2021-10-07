7bb4a9a1d69b564febc2cf0e6e9407a91f420671
JavierNunesRyuken <90431153+JavierNunesRyuken@users.noreply.github.com>
2021-10-07 15:05:12 -0300

    Add files via upload

--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -19,18 +19,49 @@ texto_2 = str(input('Ingrese la segunda palabra:\n'))
 # Compare cual de las dos palabras es mayor (alfabéticamente)
 # Imprima en pantalla según corresponda
 
+if texto_1 > texto_2 :
+    print ("el texto", texto_1, "es mayor (alfabéticamente)")
+
+else :
+    print ("el texto", texto_2, "es mayor (alfabéticamente)")
+
 # Compare cual de las dos palabras tiene mayor
 # cantidad de letras
 # Imprima en pantalla según corresponda
 
+if len(texto_1) < len(texto_2) :
+    print ("el texto ", texto_2, "tiene mayor cantidad de letras")
+else :
+    print ("el texto", texto_1, "tiene mayor cantidad de letras")
+
 # Verifique si la primera letra de la primera palabra
 # es mayor a la primera letra de la segunda palabra
 # Imprima en pantalla según corresponda
+
+if  texto_1[0] < texto_2[0] :
+    print ("el texto", texto_2, "es mayor")
+
+else :
+    print ("el texto", texto_1, "es mayor")
 
 copia_texto_1 = texto_1  # Copia de la variable texto_1
 
 # Verifique que copia_texto_1 es igual a texto_1
 # Imprima en pantalla según corresponda
 
+if copia_texto_1 == texto_1 :
+    print ("los dos textos son iguales",)
+
+else :
+    print ("los dos textos son distintos")
+
 # Verifique que copia_texto_1 es distinta a texto_2
 # Imprima en pantalla según corresponda
+
+if texto_2 != texto_1 :
+    print ("los dos textos son distintos")
+
+else :
+    print ("los dos textos son iguales")
+
+
--- ejercicios_practica/ejercicio_3.py
+++ ejercicios_practica/ejercicio_3.py
@@ -10,9 +10,15 @@
 # Ejercicios de práctica numérica
 
 # Condicionales anidados
-numero_1 = 7
-numero_2 = -2
 
+while True:
+
+    print ("ingrese primer numero\n")
+    numero_1 = int (input())
+
+    print ("ingrese segundo numero\n")
+    numero_2 = int (input())
+ 
 # Verifique si el numero_1 es mayor a 5
 #   --> En caso afirmativo, verifique si el numero_2
 #       es positivo
@@ -23,9 +29,21 @@ numero_2 = -2
 #       --> En caso afirmativo imprima en pantalla "Resp=3"
 #       --> En caso negativo imprima en pantalla "Resp=4"
 
+    if numero_1 > 5:
+        if numero_2 >= 1:
+            print ("Resp=1")
+        else :
+            print ("Resp=2")
+    elif numero_2 > 5:
+        print ("Resp=3")
+    else :
+        print ("Resp=4")
+
+
 # Verifique la calificación de un estudiante según su
 # puntaje en un examen
-puntaje = 70
+    print ("ingrese puntaje\n")
+    puntaje = int (input())
 
 # Si el puntaje es mayor igual a 90 --> imprimir A
 # Si el puntaje es mayor igual a 80 --> imprimir B
@@ -35,3 +53,45 @@ puntaje = 70
 
 # Debe imprimir en pantalla la calificacion
 # Utilizar "if" anidados
+
+    if puntaje >= 100 :
+        print ("Su resultado fue", puntaje)
+        print ("Resultado de la prueva:¨A+¨")
+        print("Sobrepasaste mis espectativas :)")
+    elif puntaje > 90 :
+        print ("Su resultado fue", puntaje)
+        print ("Resultado de la prueva:¨A¨")
+
+    elif puntaje >= 80 :
+        print ("Su resultado fue", puntaje)
+        print ("Resultado de la prueva:¨B¨")
+
+    elif puntaje >= 70 :
+        print ("Su resultado fue", puntaje)
+        print ("Resultado de la prueva:¨C¨")
+
+    elif puntaje >= 60 :
+        print ("Su resultado fue", puntaje)
+        print ("Resultado de la prueva:¨D¨")
+
+    elif (puntaje < 60) and (puntaje > 0) :
+        print ("Su resultado fue", puntaje)
+        print ("Resultado de la prueva:¨F¨")
+
+    elif puntaje <= 0:
+        print (puntaje, "no es valido")
+
+    print ("""ingrese 1 para continuar
+ingrese 2 para salir\n""")
+    numero = int (input())
+
+    if numero == 1 :
+        print ("Gracias por continuar")
+        continue
+    elif numero == 2 :
+        print ("Gracias por usar nuestra aplicacion")
+        break
+
+
+
+
